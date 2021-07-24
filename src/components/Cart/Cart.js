da89762c45e3cb08fe676ac2b41bf46937fe6729
Samuel <samkoj@hotmail.co.uk>
2021-07-24 11:34:05 +0100

    add and remove items from cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,40 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+	const cartCtx = useContext(CartContext);
+
+	const totalAmount = `$${Math.abs(cartCtx.totalAmount).toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
+
+	const removeItemHandler = (id) => {
+		cartCtx.removeItem(id);
+	};
+
+	const addItemHandler = (item) => {
+		cartCtx.addItem({ ...item, amount: 1 });
+	};
+
 	const cartItems = (
-		<ul className={classes["card-items"]}>
-			{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-				<li>{item.name}</li>
+		<ul className={classes["cart-items"]}>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					/*
+            Alternative to using anonymous function to pass params without executing.
+            The bind() method creates a new function that, when called, has its this keyword set to the provided value, 
+            with a given sequence of arguments preceding any provided when the new function is called.
+            https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Function/bind
+          */
+					onRemove={removeItemHandler.bind(null, item.id)}
+					onAdd={addItemHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	);
@@ -15,13 +44,13 @@ const Cart = (props) => {
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
-				<span>35.62</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={classes.actions}>
 				<button onClick={props.onHideCart} className={classes["button--alt"]}>
 					Close
 				</button>
-				<button className={classes.button}>Order</button>
+				{hasItems && <button className={classes.button}>Order</button>}
 			</div>
 		</Modal>
 	);
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,14 +8,44 @@ const cartDefaultState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === "ADD") {
-		// const updatedItems = state.items.concat(action.item);
-		const updatedItems = [...state.items, action.item];
 		const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+		const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+		const existingCartItem = state.items[existingCartItemIndex];
+		let updatedItems;
+
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			updatedItems = [...state.items, action.item];
+		}
+
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
 		};
 	}
+
+	if (action.type === "REMOVE") {
+		const cartItemIndex = state.items.findIndex((item) => item.id === action.id);
+		const cartItem = state.items[cartItemIndex];
+
+		let updatedState = { ...state };
+		updatedState.totalAmount -= cartItem.price;
+		if (cartItem.amount === 1) {
+			const updatedItems = updatedState.items.filter((item) => item.id !== action.id);
+			updatedState.items = updatedItems;
+		} else {
+			updatedState.items[cartItemIndex].amount -= 1;
+		}
+		return updatedState;
+	}
+
 	return cartDefaultState;
 };
 
@@ -25,7 +55,9 @@ const CartProvider = (props) => {
 	const addToCartHandler = (item) => {
 		dispatchCartAction({ type: "ADD", item: item });
 	};
-	const removeFromCartHandler = (id) => {};
+	const removeFromCartHandler = (id) => {
+		dispatchCartAction({ type: "REMOVE", id: id });
+	};
 	const cartContext = {
 		items: cartState.items,
 		totalAmount: cartState.totalAmount,
