c8e1f1c6e740231d2a71d6552d8ab2607791488f
Samuel <samkoj@hotmail.co.uk>
2021-07-25 17:43:46 +0100

    add HeaderCartButton animation on badge change

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
@@ -8,8 +8,35 @@ const HeaderCartButton = (props) => {
 	const cartTotalItems = cartCtx.items.reduce((accumulator, item) => {
 		return accumulator + item.amount;
 	}, 0);
+
+	// Max's example
+	// const [btnBadgeChanged, setBtnBadgeChanged] = useState(false);
+	// const btnClasses = `${classes.button} ${btnBadgeChanged ? classes.bump : ""}`;
+	// useEffect(() => {
+	// 	if (cartCtx.items.length === 0) return;
+	// 	setBtnBadgeChanged(true);
+	// 	const timer = setTimeout(() => {
+	// 		setBtnBadgeChanged(false);
+	// 	}, 300);
+	// 	return () => {
+	// 		clearTimeout(timer);
+	// 	};
+	// }, [cartCtx.items]);
+
+	const [btnClasses, setBtnClasses] = useState(`${classes.button}`);
+
+	useEffect(() => {
+		if (cartCtx.items.length === 0) return;
+		setTimeout(() => {
+			setBtnClasses(`${classes.button} ${classes.bump}`);
+		}, 30);
+		return () => {
+			setBtnClasses(`${classes.button}`);
+		};
+	}, [cartCtx.items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
